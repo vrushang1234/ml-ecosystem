81ce4e186de79c0eeb53b2e643c96e20bd5ce362
Steven Kuzhipala <steven.k.biji@gmail.com>
2026-07-03 12:15:54 -0700

    Changed so that adder module itself dectects for subtraction or addition, allowing us to use the negation module not only for subtraction.

--- isa/adder/alu_variable_adder.sv
+++ isa/adder/alu_variable_adder.sv
@@ -1,8 +1,4 @@
 `timescale 1ns / 1ps
-
-`include "constants.h"
-
-`timescale 1ns / 1ps
 `include "constants.h"
 
 module alu_variable_adder #(parameter N = `DATA_WIDTH) (
@@ -12,16 +8,14 @@ module alu_variable_adder #(parameter N = `DATA_WIDTH) (
     output logic [N-1:0] Sum,
     output logic  overflow
 );
-
     logic [N-1:0] operand_B;
-
+    logic [N-1:0] adder_b; //wire used to be either the original B input for addtion,
+                           //or negated B for subtraction 
     negation_module #(.N(N)) neg_inst (
         .B(B),
-        .subtract(subtract),
         .operand_B(operand_B)
     );
-
-    assign Sum = A + operand_B;
-    assign overflow = (A[N-1] == operand_B[N-1]) && (Sum[N-1] != A[N-1]);
-
+    assign adder_b = subtract ? operand_B : B;
+    assign Sum = A + adder_b;
+    assign overflow = (A[N-1] == adder_b[N-1]) && (Sum[N-1] != A[N-1]);
 endmodule
--- isa/adder/negation_module.sv
+++ isa/adder/negation_module.sv
@@ -1,20 +1,18 @@
 /* 
 Reusing the same input register for storing the 
 nagated value. 
+Always returns the Two's complement verison of input
 */
 `timescale 1ns / 1ps
 `include "constants.h" 
-
 module negation_module #(parameter N= `DATA_WIDTH )
     (
         input  logic [N-1:0] B,
-        input  logic subtract,
         output logic [N-1:0] operand_B
     );
     always_comb begin 
-        //if subtract HIGH, return two's complement of B
-        //else return B as is
-        operand_B = subtract ? (~B + 1'b1) : B ; 
+
+        operand_B =(~B + 1'b1); 
     end 
     
 endmodule
