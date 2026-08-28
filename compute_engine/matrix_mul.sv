9f0874bf004d600a491bf7c6c80cd311d852a88a
Steven Kuzhipala <steven.k.biji@gmail.com>
2026-08-27 18:35:36 -0700

    Update matrix multiplication module

--- compute_engine/matrix_mul.sv
+++ compute_engine/matrix_mul.sv
@@ -8,22 +8,22 @@ module matvec_mult #(
     //coming from the compute engine, used to make sure that the y_output doesn't use the garbage 
     input  logic signed [WIDTH-1:0] x_in     [N],
     input  logic signed [WIDTH-1:0] W_in     [M][N],
-    input  logic signed [WIDTH-1:0] bias_in  [M],
     output logic y_valid,
     output logic signed [WIDTH-1:0] y_out    [M]
 );
-
     logic v1, v2;
     logic signed [WIDTH-1:0] x_reg [N];
     logic signed [WIDTH-1:0] prod_reg [M][N];
-
+    
+    
     always_ff @(posedge clk) begin
         v1 <= data_valid;
         if (data_valid) begin
             x_reg <= x_in;
         end
     end
-
+    
+    
     always_ff @(posedge clk) begin
         v2 <= v1;
         if (v1) begin
@@ -34,12 +34,14 @@ module matvec_mult #(
             end
         end
     end
-
+    
+    
     always_ff @(posedge clk) begin
         y_valid <= v2;
         if (v2) begin
             for (int i = 0; i < M; i++) begin
-                logic signed [WIDTH-1:0] acc = bias_in[i];
+                logic signed [WIDTH-1:0] acc; 
+                acc = '0;
                 for (int k = 0; k < N; k++) begin
                     acc = acc + prod_reg[i][k];
                 end
@@ -47,5 +49,4 @@ module matvec_mult #(
             end
         end
     end
-
 endmodule
